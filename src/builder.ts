f09875cdd8830d74e6f2838f5c50e6145bd17770
Colin Dellow <cldellow@gmail.com>
2024-01-07 22:14:51 -0500

    progress on #1
    Adds `--single` mode to `mapt build`.
    
    Much of it is stubbed in and incomplete, but it sort of works as a
    proof-of-concept for nodes/ways.
    
    Two issues:
    
    1) Naive Lua code that uses globals, e.g.:
    
    ```lua
    function maybe_emit()
      ..
    end
    
    function node_function()
      maybe_emit()
    end
    ```
    
    cannot be modularized in the naive manner I was hoping. :( If another
    Lua file defines `maybe_emit`, the first file's `node_function` will end
    up calling the second one, which is incorrect.
    
    2) Perhaps as a consequence of (1), I'm hitting a tilemaker segfault.
    
    I'm somehow able to process
    https://www.openstreetmap.org/relation/13453960 such that tilemaker
    thinks it's a linestring.
    
    Naively, this makes sense: it's `type=site`, so it doesn't qualify as a
    native multipolygon or a boundary. (I think
    https://wiki.openstreetmap.org/wiki/Relation:building may be relevant?)
    
    I think because of (1), my POI code runs in the building phase, calls
    Intersects, and the world ends.
    
    I think (2) could be sidestepped if I fixed up my code to enforce
    node_keys/way_keys, and to not have the global issue.

--- src/builder.ts
+++ src/builder.ts
@@ -1,17 +1,30 @@
 import { Glob } from "bun";
-import { unlinkSync } from "node:fs";
-import { resolve } from 'path';
+import fs from "node:fs";
+import { basename, dirname, join, resolve } from 'path';
+import os from 'os';
+import json6 from 'json-6';
+
+class TerminateError extends Error {
+  constructor(exitCode) {
+    super("");
+    this.name = "TerminateError";
+    this.exitCode = exitCode;
+  }
+}
 
 export async function build(args: {
-  rootDir: string,
+  isSingle: boolean,
   pbfs: string[],
-  slices: string[]
+  slices: string[],
+  tilemakerArgs: string[]
 }) {
-  const { pbfs, slices } = args;
+  const { tilemakerArgs, isSingle, pbfs, slices } = args;
   if (pbfs.length === 0)
     throw new Error(`you must pass at least one pbf`);
 
-  if (slices.length === 0) {
+  if (slices.length === 0 || isSingle) {
+    while(slices.length > 0) slices.pop();
+
     const glob = new Glob("/slices/*.json");
     for (const file of glob.scanSync(".")) {
       const slice = file.replace(/.*[/]/, '').replace('.json', '');
@@ -19,10 +32,231 @@ export async function build(args: {
     }
   }
 
+  const tmpPrefix = resolve(join('slices', `mapt_${process.pid}_`));
+
+  try {
+    try {
+      if (isSingle)
+        return await buildSingle({ tmpPrefix, tilemakerArgs, pbfs, slices });
+
+      return await buildMany({ tmpPrefix, tilemakerArgs, pbfs, slices });
+    } finally {
+      if (process.env.KEEP_FILES !== '1') {
+        const glob = new Glob(basename(`${tmpPrefix}`) + '*');
+        for (const file of glob.scanSync(dirname(tmpPrefix))) {
+          const fqpath = resolve('slices', file);
+          fs.unlinkSync(fqpath);
+        }
+      }
+    }
+  } catch (e) {
+    if (e instanceof TerminateError)
+      process.exit(e.exitCode);
+    
+    throw e;
+  }
+}
+
+function mergeJson(acc: any, cur: any): any {
+  for (const [k, v] of Object.entries(cur)) {
+    if (typeof acc[k] === 'undefined') {
+      acc[k] = v;
+    } else if (k === 'layers') {
+      for (const [layerName, layer] of Object.entries(v)) {
+        if (acc.layers[layerName]) {
+          // This isn't _always_ an error. For example, Hiker Atlas
+          // uses a fake `parks` layer.
+          console.warn(`duplicate layer detected: ${layerName}`);
+        }
+
+        acc.layers[layerName] = layer;
+      }
+    } else if (k === 'settings') {
+      for (const [setting, value] of Object.entries(v)) {
+        if (setting === 'minzoom')
+          // Take min
+          acc.settings[setting] = Math.min(value, acc.settings[setting] ?? 999);
+        else if (setting === 'maxzoom' || setting === 'basezoom')
+          // Take max
+          acc.settings[setting] = Math.max(value, acc.settings[setting] ?? -1);
+        else if (setting === 'include_ids' || setting === 'compress' || setting == 'metadata' || setting == 'filemetadata' || setting === 'combine_below' || setting === 'name' || setting === 'version' || setting === 'description')
+          // Last writer wins
+          acc.settings[setting] = value;
+        else
+          console.warn(`unknown layer.settings key: ${setting}`);
+      }
+
+    } else {
+      console.log(`unexpected key: ${k}`);
+    }
+  }
+};
+
+async function buildSingle(args: {
+  tmpPrefix: string;
+  pbfs: string[];
+  slices: string[],
+  tilemakerArgs: string[]
+}) {
+  // We jump through a lot of hoops. The general idea is that we'd
+  // like to create a Lua file that forwards calls to node_function
+  // and friends to the versions defined in the user's main files.
+  //
+  // ...but those main files aren't expecting to be used as a module,
+  // so first we'll rewrite them to export their functions.
+  //
+  // ...and we don't want to make any assumptions about the working
+  // directory, so we create the temp files in the same folder as
+  // the main files, cleaning up after ourselves at the end.
+  //
+  // It's a bit of bookkeeping, but hopefully gives a good developer
+  // experience.
+  const { tmpPrefix, tilemakerArgs, pbfs, slices } = args;
+
+  const layerJson = {};
+
+  // Construct a single JSON file.
+  const glob = new Glob("/slices/*.json");
+  const sliceLuas = [];
+  for (const file of glob.scanSync(".")) {
+    sliceLuas.push(resolve('slices', basename(file, '.json') + '.lua'));
+    const dataString = await (Bun.file(file).text());
+    const data = json6.parse(dataString);
+    mergeJson(layerJson, data);
+  }
+
+  const jsonFile = `${tmpPrefix}config.json`;
+  fs.writeFileSync(jsonFile, JSON.stringify(layerJson, null, 2), 'utf-8');
+
+  const sliceMap = {};
+
+  // Rewrite the layer-specific Lua files to export their functions.
+  // Note that there may be some Lua files that are just library code--
+  // we shouldn't rewrite them.
+  let count = 0;
+  for (const inputLua of sliceLuas) {
+    count++;
+
+    const tmpLua = tmpPrefix + basename(inputLua);
+    const dataString = await (Bun.file(inputLua).text());
+    fs.writeFileSync(tmpLua, `${dataString}
+
+local _mapt_module = {};
+_mapt_module.node_keys = node_keys or nil;
+_mapt_module.way_keys = way_keys or nil;
+_mapt_module.init_function = init_function or function(name) end;
+_mapt_module.exit_function = exit_function or function() end;
+_mapt_module.node_function = node_function or function() end;
+_mapt_module.way_function = way_function or function() end;
+_mapt_module.relation_scan_function = relation_scan_function or function() end;
+_mapt_module.relation_function = relation_function or function() end;
+return _mapt_module;
+`, 'utf-8');
+    sliceMap[inputLua] = {
+      name: basename(tmpLua, '.lua'),
+    };
+
+    //if (count >= 8) break;
+  }
+
+  // Write a driver Lua file that proxies to the layer-specific functions.
+  const driverFile = tmpPrefix + 'driver.lua';
+
+  const modnames = Object.values(sliceMap).map(x => x.name);
+
+  const generateInvokes = (what: string) => {
+    return modnames.map(x => `  ${x}.${what}\n`).join('');
+  }
+
+  const driverString = `
+${modnames.map(x => `${x} = require('${x}');\n`).join('')}
+
+-- TODO: initialize node_keys / way_keys
+
+function init_function(name)
+${generateInvokes('init_function(name);')}
+end
+
+function exit_function()
+${generateInvokes('exit_function();')}
+end
+
+function attribute_function(attr, layer)
+  -- TODO: forward to modules
+  return {}
+end
+
+function node_function()
+  -- TODO: respect node_keys
+  ${generateInvokes('node_function();')}
+end
+
+function way_function()
+  -- TODO: respect way_keys
+  ${generateInvokes('way_function();')}
+end
+
+function relation_scan_function()
+  -- TODO: give proxied modules a chance to say yes
+  return false
+end
+
+function relation_function()
+  ${generateInvokes('relation_function(); RestartRelations();')}
+end
+`;
+
+  console.log(driverString);
+  fs.writeFileSync(driverFile, driverString, 'utf-8');
+
+  const tileFile = resolve(`tiles.pmtiles`);
+
+  try {
+    fs.unlinkSync(tileFile);
+  } catch (e) {
+    // ignored
+  }
+
+  const rv = Bun.spawnSync([
+    ...(process.env.DEBUG === '1' ? ['gdb', '--args'] : []),
+    'tilemaker',
+    ...pbfs.flatMap(pbf => ['--input', pbf]),
+    '--output',
+    tileFile,
+    `--config`,
+    jsonFile,
+    `--process`,
+    driverFile,
+    ...tilemakerArgs
+  ], {
+    cwd: 'slices',
+    stdin: 'inherit',
+    stdout: 'inherit',
+    stderr: 'inherit',
+  });
+
+  if (rv.exitCode !== 0)
+    throw new TerminateError(rv.exitCode);
+
+}
+
+async function buildMany(args: {
+  tmpPrefix: string;
+  pbfs: string[];
+  slices: string[],
+  tilemakerArgs: string[]
+}) {
+  const { tmpPrefix, tilemakerArgs, pbfs, slices } = args;
   for (const slice of slices) {
     const tileFile = resolve(`${slice}.pmtiles`);
 
-    unlinkSync(tileFile);
+    try {
+      fs.unlinkSync(tileFile);
+    } catch (e) {
+      // ignored
+    }
+
+    // TODO: convert the JSON file from json6 to vanilla JSON
 
     const rv = Bun.spawnSync([
       'tilemaker',
@@ -33,6 +267,7 @@ export async function build(args: {
       `${slice}.json`,
       `--process`,
       `${slice}.lua`,
+      ...tilemakerArgs
     ], {
       cwd: 'slices',
       stdout: 'inherit',
@@ -40,8 +275,6 @@ export async function build(args: {
     });
 
     if (rv.exitCode !== 0)
-      process.exit(rv.exitCode);
-
-
+      throw new TerminateError(rv.exitCode);
   }
 }
--- src/cli.ts
+++ src/cli.ts
@@ -11,14 +11,23 @@ let args = Bun.argv.slice(2);
 async function entrypoint() {
   if (args[0] === 'build') {
     args = args.slice(1);
+    let tilemakerArgs: string[] = [];
 
+    const hasTilemakerArgs = args.indexOf('--');
+    if (hasTilemakerArgs >= 0) {
+      tilemakerArgs = args.slice(hasTilemakerArgs + 1);
+      args = args.slice(0, hasTilemakerArgs);
+    }
+    const isSingle = args.indexOf('--single') >= 0;
+    args = args.filter(x => x !== '--single');
     const pbfs = args.filter(x => x.endsWith('.pbf'));
     const slices = args.filter(x => !x.endsWith('.pbf'));
 
     return build({
-      rootDir,
       pbfs,
       slices,
+      isSingle,
+      tilemakerArgs
     });
   } else if (args[0] === 'serve') {
     const port = 8081; // TODO: make this configurable
